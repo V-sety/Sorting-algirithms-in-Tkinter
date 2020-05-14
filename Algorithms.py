c9258583c30dc8e1c127ab2a93e9b0c49c1ad1fc
Olga S <Simon.Volhv@gmail.com>
2020-05-14 20:53:49 +0400

    Heap Sort added

--- Algorithms.py
+++ Algorithms.py
@@ -1,72 +1,122 @@
 import time
+
+
 def bubbleSort(array, drawArr):
-	if len(array) == 1:
-		return array
-	l = len(array)-1
-	while l !=0:
-		for i in range(l):
-			if array[i] > array[i+1]:
-				array[i],array[i+1] = array[i+1],array[i]
-				drawArr(array, ["pink" if x ==i or x ==i+1 else "dark red" for x in range(len(array))])
-				time.sleep(0.3)
-		l = l-1
-	drawArr(array, ["pink" for _ in range(len(array))])
-	
+    if len(array) == 1:
+        return array
+    l = len(array)-1
+    while l != 0:
+        for i in range(l):
+            if array[i] > array[i+1]:
+                array[i], array[i+1] = array[i+1], array[i]
+                drawArr(array, ["pink" if x == i or x == i +
+                                1 else "dark red" for x in range(len(array))])
+                time.sleep(0.3)
+        l = l-1
+    drawArr(array, ["pink" for _ in range(len(array))])
+
 
 def insertionSort(array, drawArr):
-	for i in range(1, len(array)):
-		x = array[i]
-		j=i
-		while x < array [j-1] and j > 0:
-			array[j]=array[j-1]
-			drawArr(array, ["pink" if n ==i or n == j else "dark red" for n in range(len(array))])
-			time.sleep(0.3)
-			j = j-1
-		array[j] = x
-	drawArr(array, ["pink" for _ in range(len(array))])
+    for i in range(1, len(array)):
+        x = array[i]
+        j = i
+        while x < array[j-1] and j > 0:
+            array[j] = array[j-1]
+            drawArr(array, ["pink" if n == i or n ==
+                            j else "dark red" for n in range(len(array))])
+            time.sleep(0.3)
+            j = j-1
+        array[j] = x
+    drawArr(array, ["pink" for _ in range(len(array))])
+
 
 def selectionSort(array, drawArr):
-	for j in range(len(array)):
-		m = 0
-		x = array[j]
-		for i in range(j,len(array)):
-			drawArr(array, ["pink" if n ==i or n == j else "dark red" for n in range(len(array))])
-			time.sleep(0.3)
-			if array[i] < x:
-				x = array[i]
-				m = i
-		if m !=0: 
-			array[m],array[j] = array[j],array[m]
-	drawArr(array, ["pink" for _ in range(len(array))])
+    for j in range(len(array)):
+        m = 0
+        x = array[j]
+        for i in range(j, len(array)):
+            drawArr(array, ["pink" if n == i or n ==
+                            j else "dark red" for n in range(len(array))])
+            time.sleep(0.3)
+            if array[i] < x:
+                x = array[i]
+                m = i
+        if m != 0:
+            array[m], array[j] = array[j], array[m]
+    drawArr(array, ["pink" for _ in range(len(array))])
+
 
 def quickSort(array, drawArr):
-	helper(array, 0 , len(array) - 1,drawArr)
-	
-def helper(array,start,end,drawArr):
-	if start >= end:
-		return 
-	pivot = start
-	left = start + 1
-	right = end
-	while left <= right:
-		if array[left] > array[pivot] and array[right] < array[pivot]:
-			array[left],array[right] = array[right],array[left]
-			drawArr(array, ["pink" if n ==left or n == right or n == pivot else "dark red" for n in range(len(array))])
-			time.sleep(0.3)
-		if array[left] <= array[pivot]:
-			drawArr(array, ["pink" if n ==left or n == right or n == pivot else "dark red" for n in range(len(array))])
-			time.sleep(0.3)
-			left +=1
-		if array[right] >= array[pivot]:
-			drawArr(array, ["pink" if n ==left or n == right or n == pivot else "dark red" for n in range(len(array))])
-			time.sleep(0.3)
-			right -=1
-	array[pivot],array[right] = array[right],array[pivot]
-	leftIsSmaller = (right - 1) - start < end - (right + 1)
-	if leftIsSmaller:
-		helper(array, start , right - 1,drawArr)
-		helper(array, right + 1, end, drawArr)
-	else:
-		helper(array, right + 1, end, drawArr)
-		helper(array, start , right - 1, drawArr)
-	drawArr(array, ["pink" for _ in range(len(array))])
+    helper(array, 0, len(array) - 1, drawArr)
+
+
+def helper(array, start, end, drawArr):
+    if start >= end:
+        return
+    pivot = start
+    left = start + 1
+    right = end
+    while left <= right:
+        if array[left] > array[pivot] and array[right] < array[pivot]:
+            array[left], array[right] = array[right], array[left]
+            drawArr(array, ["pink" if n == left or n == right or n ==
+                            pivot else "dark red" for n in range(len(array))])
+            time.sleep(0.3)
+        if array[left] <= array[pivot]:
+            drawArr(array, ["pink" if n == left or n == right or n ==
+                            pivot else "dark red" for n in range(len(array))])
+            time.sleep(0.3)
+            left += 1
+        if array[right] >= array[pivot]:
+            drawArr(array, ["pink" if n == left or n == right or n ==
+                            pivot else "dark red" for n in range(len(array))])
+            time.sleep(0.3)
+            right -= 1
+    array[pivot], array[right] = array[right], array[pivot]
+    leftIsSmaller = (right - 1) - start < end - (right + 1)
+    if leftIsSmaller:
+        helper(array, start, right - 1, drawArr)
+        helper(array, right + 1, end, drawArr)
+    else:
+        helper(array, right + 1, end, drawArr)
+        helper(array, start, right - 1, drawArr)
+    drawArr(array, ["pink" for _ in range(len(array))])
+
+
+def heapSort(array, drawArr):
+    buildHeap(array, drawArr)
+    sortBound = len(array)
+    for i in range(sortBound):
+        array[0], array[sortBound - 1] = array[sortBound - 1], array[0]
+        drawArr(array, ["pink" if n == 0 or n == sortBound -
+                        1 else "dark red" for n in range(len(array))])
+        sortBound = sortBound - 1
+        siftDown(array, 0, drawArr, sortBound)
+    drawArr(array, ["pink" for _ in range(len(array))])
+
+
+def buildHeap(array, drawArr):
+    firstParentInd = (len(array) - 2) // 2
+    for i in range(firstParentInd, -1, -1):
+        drawArr(array, [
+                "pink" if n == firstParentInd else "dark red" for n in range(len(array))])
+        siftDown(array, i, drawArr, arrLen=len(array))
+
+
+def siftDown(array, curInd, drawArr, arrLen):
+    while 2*curInd + 1 < arrLen:
+        childOneInd = 2*curInd + 1
+        if 2*curInd + 2 < arrLen:
+            childTwoInd = 2*curInd + 2
+            if array[childOneInd] > array[childTwoInd]:
+                indToCompare = childOneInd
+            else:
+                indToCompare = childTwoInd
+        else:
+            indToCompare = childOneInd
+        if array[curInd] < array[indToCompare]:
+            array[curInd], array[indToCompare] = array[indToCompare], array[curInd]
+            drawArr(array, ["pink" if n == curInd or n ==
+                            indToCompare else "dark red" for n in range(len(array))])
+            time.sleep(0.2)
+        curInd = indToCompare
--- appT.py
+++ appT.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 import random
-from Algorithms import bubbleSort, insertionSort, selectionSort, quickSort
+from Algorithms import bubbleSort, insertionSort, selectionSort, quickSort, heapSort
 
 pady = 8
 padx = 8
@@ -15,6 +15,7 @@ selectedAlg = StringVar()
 arr = []
 maxElem = 0
 
+
 def drawArr(arr, colors):
     canvas.delete("all")
     c_height = 480
@@ -24,20 +25,21 @@ def drawArr(arr, colors):
     spacing = 10
     for i, height in enumerate(arr):
         x0 = (i * x_width) + offset + spacing
-        y0 =  c_height - (c_height - 50)*(height/maxElem)
+        y0 = c_height - (c_height - 50)*(height/maxElem)
         x1 = x0 + x_width
-        y1 = c_height 
-        canvas.create_rectangle(x0, y0, x1, y1, fill = colors[i])
-        canvas.create_text(x0 + x_width/2, y0 - 15, text = str(arr[i]))
+        y1 = c_height
+        canvas.create_rectangle(x0, y0, x1, y1, fill=colors[i])
+        canvas.create_text(x0 + x_width/2, y0 - 15, text=str(arr[i]))
 
     root.update_idletasks()
+
 
 def generateArr():
     try:
         minVal = int(minEntry.get())
     except:
         minVal = 10
-    try:    
+    try:
         maxVal = int(maxEntry.get())
     except:
         maxVal = 150
@@ -62,44 +64,57 @@ def generateArr():
     drawArr(arr, ["dark red" for _ in range(len(arr))])
     return arr, maxElem
 
+
 def startSort():
     global arr
-    if algMenu.get() == "Bubble Sort":
-        bubbleSort(arr, drawArr) 
-    if algMenu.get() == "Insertion Sort":
+    global selectedAlg
+    selectedAlg = algMenu.get()
+    if selectedAlg == "Bubble Sort":
+        bubbleSort(arr, drawArr)
+    if selectedAlg == "Insertion Sort":
         insertionSort(arr, drawArr)
-    if algMenu.get() == "Selection Sort":
+    if selectedAlg == "Selection Sort":
         selectionSort(arr, drawArr)
-    if algMenu.get() == "Quick Sort":
+    if selectedAlg == "Quick Sort":
         quickSort(arr, drawArr)
-
-UI_frame = Frame(root, width = 800, height= 200, bg="pink")
-UI_frame.grid(row = 0, column = 0, padx = 10, pady = 5)
-
-canvas = Canvas(root, width = 800, height = 480, bg = "white")
-canvas.grid(row = 1, column = 0, padx = 10, pady = 5)
+    if selectedAlg == "Heap Sort":
+        heapSort(arr, drawArr)
 
 
-#row0
-Label(UI_frame, text ="Choose algorithm: ", bg ="pink").grid(row = 0, column = 0, padx = padx, pady = pady, sticky =W)
-algMenu = ttk.Combobox(UI_frame, textvariable=selectedAlg, values = ["Bubble Sort", "Insertion Sort", "Selection Sort", "Quick Sort"])
-algMenu.grid(row = 0, column = 1, padx = padx, pady = pady)
+UI_frame = Frame(root, width=800, height=200, bg="pink")
+UI_frame.grid(row=0, column=0, padx=10, pady=5)
+
+canvas = Canvas(root, width=800, height=480, bg="white")
+canvas.grid(row=1, column=0, padx=10, pady=5)
+
+
+# row0
+Label(UI_frame, text="Choose algorithm: ", bg="pink").grid(
+    row=0, column=0, padx=padx, pady=pady, sticky=W)
+algMenu = ttk.Combobox(UI_frame, textvariable=selectedAlg, values=[
+                       "Bubble Sort", "Insertion Sort", "Selection Sort", "Quick Sort", "Heap Sort"])
+algMenu.grid(row=0, column=1, padx=padx, pady=pady)
 algMenu.current(0)
 
-Button(UI_frame, text = "Generate array", command = generateArr, bg = "dark red").grid(row = 0, column = 2,padx = padx, pady = pady)
-Button(UI_frame, text = "Start", command = startSort, bg = "dark red").grid(row = 0, column = 3,padx = padx, pady = pady)
-#row1
-Label(UI_frame, text ="Array size ", bg ="pink").grid(row = 1, column = 0, padx = padx, pady = pady, sticky =W)
+Button(UI_frame, text="Generate array", command=generateArr,
+       bg="dark red").grid(row=0, column=2, padx=padx, pady=pady)
+Button(UI_frame, text="Start", command=startSort, bg="dark red").grid(
+    row=0, column=3, padx=padx, pady=pady)
+# row1
+Label(UI_frame, text="Array size ", bg="pink").grid(
+    row=1, column=0, padx=padx, pady=pady, sticky=W)
 sizeEntry = Entry(UI_frame)
-sizeEntry.grid(row = 1, column = 1, padx = 5, pady = 5, sticky =W)
+sizeEntry.grid(row=1, column=1, padx=5, pady=5, sticky=W)
 
-Label(UI_frame, text ="Minimum value ", bg ="pink").grid(row = 1, column = 2, padx = padx, pady = pady, sticky =W)
+Label(UI_frame, text="Minimum value ", bg="pink").grid(
+    row=1, column=2, padx=padx, pady=pady, sticky=W)
 minEntry = Entry(UI_frame)
-minEntry.grid(row = 1, column = 3, padx = 5, pady = 5, sticky =W)
+minEntry.grid(row=1, column=3, padx=5, pady=5, sticky=W)
 
-Label(UI_frame, text ="Maximim value ", bg ="pink").grid(row = 1, column = 4, padx = padx, pady = pady, sticky =W)
+Label(UI_frame, text="Maximim value ", bg="pink").grid(
+    row=1, column=4, padx=padx, pady=pady, sticky=W)
 maxEntry = Entry(UI_frame)
-maxEntry.grid(row = 1, column = 5, padx = padx, pady = pady, sticky =W)
+maxEntry.grid(row=1, column=5, padx=padx, pady=pady, sticky=W)
 root.mainloop()
 
 # add stop button, heap sort, merge sort
